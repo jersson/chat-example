cec8b7dafba47d23a10711dae652eea55a9af438
Jersson Dongo <Jersson@gmail.com>
2020-11-01 17:02:27 -0500

    feat: add required log messages

--- index.js
+++ index.js
@@ -8,8 +8,15 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+  console.log('a user connected');
+
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+    console.log('chat message:', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
   });
 });
 
