8c32c145cfc2d772e7c7963caa6f3b9a3b710476
Jersson Dongo <Jersson@gmail.com>
2020-11-07 23:08:29 -0500

    fix: remove linter issues

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
